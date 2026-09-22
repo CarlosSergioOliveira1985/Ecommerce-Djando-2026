77999a3d09d13cf55bdd99ee074daddfc39b2827
Carlos Sergio de Oliveira <carlossergio_20@hotmail.com>
2026-09-22 11:04:09 -0300

    Criando pedidos e itens do pedido no Admin

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .import models
 
 # Register your models here.
+
+class ItemPedidoInline(admin.TabularInline):
+
+    model = models.ItemPedido
+    extra = 1
+    
+class PedidoAdimin(admin.ModelAdmin):
+    inlines = [
+        ItemPedidoInline
+        ]
+
+
+admin.site.register(models.Pedido, PedidoAdimin)
+admin.site.register(models.ItemPedido)
